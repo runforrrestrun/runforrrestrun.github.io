d1a7c02d5cdc5d9f83576daf18f0e4c0be73f9e0
runforrrestrun <92750257+runforrrestrun@users.noreply.github.com>
2024-07-28 17:48:06 +0100

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,28 @@
+"use strict";
+const modal = document.querySelector(".popUp");
+const overlay = document.querySelector(".overlay");
+const btnCloseModat = document.querySelector(".close-popUp");
+const btnsOpenModal = document.querySelectorAll(".show-popUp");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModat.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
