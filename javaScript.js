9c8f3739d1ae29daf5987e752d321e7c2c466fbd
runforrrestrun <92750257+runforrrestrun@users.noreply.github.com>
2024-08-08 17:26:44 +0100

    Add files via upload

--- javaScript.js
+++ javaScript.js
@@ -57,3 +57,28 @@ function openCity(evt, cityName) {
   evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+//
+//
+//
+// CASIOS INFO TAB
+function Buttons(evt, button) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent-info");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks-info");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(button).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
