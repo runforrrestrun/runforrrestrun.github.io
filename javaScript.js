57973549e3c9f56d0fff4c048c3e93a840119b74
runforrrestrun <92750257+runforrrestrun@users.noreply.github.com>
2024-10-15 13:55:04 +0300

    Add files via upload

--- javaScript.js
+++ javaScript.js
@@ -51,42 +51,6 @@ function myFunction2() {
 
 //
 // news slidebar
-// let currentSlide = 0;
-// const slides = document.querySelectorAll(".news-item");
-// const totalSlides = slides.length;
-
-// document.querySelector(".forth-btn").addEventListener("click", () => {
-//   moveToNextSlide();
-// });
-
-// document.querySelector(".back-btn").addEventListener("click", () => {
-//   moveToPreviousSlide();
-// });
-
-// function moveToNextSlide() {
-//   if (currentSlide < totalSlides - 1) {
-//     currentSlide++;
-//   } else {
-//     currentSlide = 0; // Loop back to the first slide
-//   }
-//   updateSlidePosition();
-// }
-
-// function moveToPreviousSlide() {
-//   if (currentSlide > 0) {
-//     currentSlide--;
-//   } else {
-//     currentSlide = totalSlides - 1; // Loop back to the last slide
-//   }
-//   updateSlidePosition();
-// }
-
-// function updateSlidePosition() {
-//   const sliderWidth = document.querySelector(".news-slider").offsetWidth;
-//   document.querySelector(".news-wrapper").style.transform = `translateX(-${
-//     currentSlide * sliderWidth
-//   }px)`;
-// }
 
 let currentSlide = 0;
 const slides = document.querySelectorAll(".news-item");
