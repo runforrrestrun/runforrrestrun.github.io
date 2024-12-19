70f2b6737acb0febfb6b6e85252861259b9aac60
runforrrestrun <92750257+runforrrestrun@users.noreply.github.com>
2024-12-19 13:49:54 +0200

    Add files via upload

--- javaScript.js
+++ javaScript.js
@@ -334,16 +334,6 @@ document.addEventListener("DOMContentLoaded", () => {
     bonusDropdownContent.classList.toggle("show");
   }
 
-  // Prevent swipe gestures from going back when side navigation is open
-  function preventBackSwipe(event) {
-    if (
-      sideNavigation.classList.contains("open") &&
-      !sideNavigation.contains(event.target)
-    ) {
-      event.preventDefault(); // Prevent swipe gesture to go back
-    }
-  }
-
   // Attach event listeners
   toggleButton.addEventListener("click", toggleNavigation);
   closeButton.addEventListener("click", toggleNavigation);
@@ -351,9 +341,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   reviewsDropdown.addEventListener("click", toggleReviewsDropdown);
   bonusDropdown.addEventListener("click", toggleBonusDropdown);
-
-  // Add touch event listener to prevent swipe gestures when side navigation is open
-  document.addEventListener("touchstart", preventBackSwipe, { passive: false });
 });
 
 console.log("JavaScript is loaded!");
